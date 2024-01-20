3763effaac7483872805b2d27d76da4cd463ad3e
Aya Debbagh <adebb027@uottawa.ca>
2024-01-20 15:25:38 -0500

    Feat: added kill zombie

---
+++ Assets/Scripts/ZombieType.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieType : MonoBehaviour
+
+{
+    public string zombieType = "Normal";
+    public float health = 3f;
+    public float damage;
+    GunSpecs gunSpecs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        damage = GunSpecs.bulletDamage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision) 
+        {
+            if (collision.gameObject.CompareTag("Bullet")) {
+                health -= damage;
+                Debug.Log("Health: " + health.ToString());
+            }
+            
+        }
+}
